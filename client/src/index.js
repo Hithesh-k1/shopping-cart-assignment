5ee759c454575ab5c93846b0a115b0616233c324
hithesh-k1 <hitheshk27@gmail.com>
2022-07-03 14:32:24 +0530

    upgraded to react 18

--- client/src/index.js
+++ client/src/index.js
@@ -6,13 +6,17 @@ import reportWebVitals from './reportWebVitals';
 import "react-toastify/dist/ReactToastify.css";
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+const container = document.getElementbyId("app");
+// create a root
+const root = ReactDOM.createRoot(container);
+// render app to root
+root.render(<App />);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
